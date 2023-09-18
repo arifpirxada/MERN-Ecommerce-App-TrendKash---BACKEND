0aff0a883b34074cae4785716b161901f0fb3193
arifpirxada <arifpirxada@gmail.com>
2023-09-18 13:11:29 +0530

    update - date field added in contact collection

--- src/models/contact.js
+++ src/models/contact.js
@@ -8,6 +8,10 @@ const contactSchema = new mongoose.Schema({
     message: {
         type: String,
         required: true
+    },
+    date: {
+        type: String,
+        required: true
     }
 })
 
