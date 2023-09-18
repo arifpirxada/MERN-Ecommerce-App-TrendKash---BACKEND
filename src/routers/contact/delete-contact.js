6c62f982aa11d0d58f2264208259c8898c8eedff
arifpirxada <pirxadaarif@gmail.com>
2023-09-18 13:03:41 +0530

    update - contact functionality, deleted trash images, uploaded new images using fetch from the front-end

---
+++ src/routers/contact/delete-contact.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const router = new express.Router()
+const contact = require("../../models/contact")
+
+router.delete("/delete-contact", async (req, res) => {
+    try {
+        const _id = req.body.id
+        await contact.findByIdAndDelete(_id)
+        res.status(200).json({ message: 'Deletion successful' })
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({ message: 'Internal server error' })
+    }
+})
+
+module.exports = router
