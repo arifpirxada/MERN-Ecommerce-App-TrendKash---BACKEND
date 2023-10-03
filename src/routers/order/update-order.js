e3c3ecaa7ed60024f74c8694fd52df3b3b01afe8
arifpirxada <pirxadaarif@gmail.com>
2023-10-03 22:35:47 +0530

    Fix - Saved log of status change while updating order status

--- src/routers/order/update-order.js
+++ src/routers/order/update-order.js
@@ -1,17 +1,27 @@
-const express = require("express")
-const router = new express.Router()
-const auth = require("../../middleware/auth")
-const order = require("../../models/order")
+const express = require("express");
+const router = new express.Router();
+const order = require("../../models/order");
+const moment = require("moment");
+
+const currentDate = moment();
 
 router.patch("/update-order", async (req, res) => {
-    try {
-        const _id = req.body.id
-        await order.findByIdAndUpdate(_id, {$set: {status: req.body.status}})
-        res.status(200).json({ message: 'Updation successful' })
-    } catch (e) {
-        console.log(e)
-        res.status(500).json({ message: 'Internal server error' })
-    }
-})
+  try {
+    const _id = req.body.id;
+    await order.findByIdAndUpdate(_id, {
+      $set: { status: req.body.status },
+      $push: {
+        history: {
+          status: req.body.status,
+          date: currentDate.format("DD-MM-YYYY HH:mm:ss"),
+        },
+      },
+    });
+    res.status(200).json({ message: "Updation successful" });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
-module.exports = router
+module.exports = router;
